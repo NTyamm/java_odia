a976273eca1ce90777b16713251f10dd5a1bf08c
NTyamm <euodia913@gmail.com>
2021-10-27 16:50:59 +0900

    1-5 : 문자열 예제

---
+++ odia/src/day1/StringEx1.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		/* 문자열 String (앞글자가 대문자>클래스)
+		 */
+		String str1;
+		String str2 = "Hello";
+		System.out.println(str2);
+		str1 = str2 + "World!"
+		System.out.println(str1);
+	}
+
+}
