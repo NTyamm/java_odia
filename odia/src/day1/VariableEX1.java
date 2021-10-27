5da064ce093f872148183fb302328256d1b8d04a
NTyamm <euodia913@gmail.com>
2021-10-27 16:49:43 +0900

    1-1 : 변수 선언 예제

---
+++ odia/src/day1/VariableEX1.java
@@ -0,0 +1,39 @@
+package day1;
+
+public class VariableEX1 {
+	/**
+	 *  */
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			// 한줄 주석
+			/* 여러줄 주석
+			 * 주석
+			 * */
+		 	/* 성적을 관리하는 변수를 선언하려고 합니다. 성적은 A,B,C,D,E,F로 관리
+		 	 * 이 성적은 관리하기 위한 변수를 써 보세요
+		 	 * */
+		 char score1;
+		 char score2 = 'F';
+		 //나이를 관리하는 변수를 선언해 보세요
+		 int age1;
+		 int age2 = 1;
+		 //성적의 평균을 관리하는 변수를 선언해 보세요
+		 double avg1;
+		 double avg2 = 0.0;
+		 float avg3 = 0.0f;
+		 
+		 //성인인지 아닌지를 판별하기 위한 변수를 선언해 보세요
+		 boolean isAdlt1;
+		 boolean isAdlt2 = true;
+		 
+		 int num1 = 1000;
+		 //byte num2 = 1000; // 에러발생. 자릿수가 안맞음
+		 
+		 score1 = 'A';
+		 //System.out.println(isAult1); //에러발생; 변수초기화를 하지않아서
+		
+	}
+
+}
