38e7ccc8a51451c958bbcf472e41d9e0f128fd61
NTyamm <euodia913@gmail.com>
2021-10-29 17:35:14 +0900

    3-9 For Multiple 예제

---
+++ odia/src/Day3/ForMultipleEx1.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class ForMultipleEx1 {
+
+	public static void main(String[] args) {
+		/*구구단 7단을 출력하는 코드를 작성하세요.
+		 * 7 x 1 = 1
+		 * 7 x 2
+		 */
+		int i;
+		int num = 5;
+		for( i = 1 ; i <= 9 ; i++) {
+			System.out.println( "num x " + i + " = " + num * i);
+	}
+
+}
+}
