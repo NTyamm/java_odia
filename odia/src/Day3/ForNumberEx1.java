df9fb988b7fe07a13104080d27843e5f6d623b87
NTyamm <euodia913@gmail.com>
2021-10-29 17:35:01 +0900

    3-8 For Number 예제

---
+++ odia/src/Day3/ForNumberEx1.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class ForNumberEx1 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 코드를 for문으로 작성하세요.
+		 */
+		int i;
+		for(i = 1 ; i <=5 ; i++ ) {
+			System.out.println(i);
+		}
+	}
+
+}
