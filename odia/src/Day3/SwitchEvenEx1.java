7f13d8abe7b00392be2d2e3230b03fad7f23fd45
NTyamm <euodia913@gmail.com>
2021-10-29 17:33:04 +0900

    3-3 Switch Even 예제

---
+++ odia/src/Day3/SwitchEvenEx1.java
@@ -0,0 +1,22 @@
+package Day3;
+
+import com.sun.org.apache.bcel.internal.generic.SWITCH;
+
+public class SwitchEvenEx1 {
+
+	public static void main(String[] args) {
+		/*Switch문을 이용하여 정수의 홀짝 여부를 판별하는 코드를 작성하세요.
+	    */
+	int num = 10;
+	Switch (num % 2) {
+		case 0:
+			System.out.println(num + "는 짝수");
+			break;
+		default:
+			System.out.println(num + "는 홀수");
+	}
+	}
+
+}
+
+Switch
