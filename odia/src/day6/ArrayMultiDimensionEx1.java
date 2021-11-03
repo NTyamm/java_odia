34b32c880d706fbdaa04d9dedf432e38223dda8d
odia <euodia913@gmail.com>
2021-11-03 17:18:48 +0900

    6-9 다층배열 예제

---
+++ odia/src/day6/ArrayMultiDimensionEx1.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class ArrayMultiDimensionEx1 {
+
+	public static void main(String[] args) {
+		/* 구구단 2단에서 9단 전체를 배열에 저장하여 출력하는 코드를 작성하세요.
+		 */
+		//0번지를 사용 안하기 위해. 사용 안하는 이유 -> 편하게 보기 위해
+		int [][] multipleTable = new int [10][10];
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				multipleTable[i][j] = i*j;
+				System.out.println(i + " x " + j + " = " + multipleTable[i][j]);
+			}
+		}
+		
+	}
+
+}
