7da68f0ab29e834a3c2d5f49cf5217cabb001c38
NTyamm <euodia913@gmail.com>
2021-11-04 17:15:44 +0900

    7-9 Method 매개변수예제

---
+++ odia/src/day7/MethodVariableEx1.java
@@ -0,0 +1,19 @@
+package day7;
+
+public class MethodVariableEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10, num2 = 20;
+		int result = sum(num1,num2);
+		System.out.println(num1 + "+" + num2 + "=" + result);
+	}
+	public static int sum(int num1, int num2) {
+		System.out.println("sum apthem > num1 : " + num1 + ",num2 : " + num2);
+		int tmp = num1;
+		num1 = num2;
+		num2 = tmp;
+		System.out.println("sum 메소드 > num1 : " + num1 + ", num2" + num2);
+		return num1 + num2;
+	}
+}
