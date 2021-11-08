d12c67acd7d0ddd8c41ad33265a020992e685968
NTyamm <euodia913@gmail.com>
2021-11-08 17:36:09 +0900

    9-8 클래스 예제5

---
+++ odia/src/day9/ClassEx5.java
@@ -0,0 +1,19 @@
+package day9;
+
+import day9.ClassEx3.HighSchoolStudent;
+
+public class ClassEx5 {
+
+	public static void main(String[] args) {
+		/* 학생 1명의 정보를 관리하는 예제 : 클래스 사용 x */
+		int size = 5;
+		String sName[] = new String [4], name [] = new String[size];
+		int grade[] = new int[size] , classNum[] = new int[size], num[] = new int[size];
+		
+		/* 학생 1 명의 정보를 관리하는 예제 : 클래스 사용 HighSchoolStudent*/
+		HighSchoolStudent std[] = new HighSchoolStudent[size];
+	
+
+	}
+
+}
