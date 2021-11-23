3e3c2ca292b1b31096a9f5390311f9deed44f9ef
NTyamm <euodia913@gmail.com>
2021-11-23 17:29:08 +0900

    20-2 CRUD 예제 1~3

---
+++ 2일차 update 예제.sql
@@ -0,0 +1,14 @@
+update student 
+	set 
+		st_id = 'kangkidong', 
+		st_pw ='kangkidongpw' 
+	where
+		st_num = '2021160001';
+        
+update student 
+	set 
+		st_id = 'kangkidong', 
+		st_pw ='kangkidongpw' 
+	where
+		st_name = '유관순';
+select*from university_odia.student;
