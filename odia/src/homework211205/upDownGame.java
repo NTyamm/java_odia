7c42a415566b07aec5dbfdefea4bfcc3c296aefb
NTyamm <euodia913@gmail.com>
2021-12-05 20:07:15 +0900

    12월첫째주숙제

---
+++ odia/src/homework211205/upDownGame.java
@@ -0,0 +1,32 @@
+package homework211205;
+
+public class upDownGame {
+
+	public static void main(String[] args) {
+		/* 랜덤으로 숫자를 만든 후 만들어진 숫자를 맞추는 게임
+		   숫자 범위는 1~100 사이
+		   예시(랜덤으로 만들어진 수 49)
+		   정수:5o
+		   down
+		   정수:25
+		   up
+		   정수:40
+		   up
+		   정수:49
+		   정답
+		*/
+		
+		//컴퓨터가 랜덤한 숫자를 만듬
+		//사용자가 랜덤한 숫자를 입력함
+		//while 문이 나을 것 같음. case 1, 2, 3, default로
+		// if 사용자가 입력한 숫자 > 컴퓨터가 만든 숫자
+		// println('down');
+		// if 사용자가 입력한 숫자< 컴퓨터가 만든 숫자
+		// println('up');
+		// if 사용자가 입력한 숫자== 컴퓨터가 만든 숫자
+				//break; println('정답입니다.');
+		
+
+	}
+
+}
